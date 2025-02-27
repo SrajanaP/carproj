fac41ab725ffa63d97c527f89b6b129d0b165657
SrajanaP <146545311+SrajanaP@users.noreply.github.com>
2025-02-27 10:11:44 +0530

    Add files via upload

---
+++ vehicle.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vehicle Management</title>
+    <link href="vehiclecs.css" rel="stylesheet">
+</head>
+<body>
+    <div class="nav">
+        <a href="#">HOME</a>
+        <a href="#">Admin</a>
+        <a href="#">Add</a>
+        <a href="#about">about</a>
+    </div>
+    <div class="vehiclelist">
+        <div class="card">
+            <div class="innercard1">
+                <img src="car2.jpg" alt="">
+            </div>
+           
+            <div class="innercard2">
+                <p class="carname"><span class="start">BWM</span><span class="end">$1000/day</span></p>
+                <p class="carname">Year : 2021 </p>
+                <p class="carname">Fuel type : Diesel</p>
+                <p class="carname">Location : Bangalore <button>view</button></p>
+                
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="innercard1">
+                <img src="car2.jpg" alt="">
+            </div>
+           
+            <div class="innercard2">
+                <p class="carname"><span class="start">BWM</span><span class="end">$1000/day</span></p>
+                <p class="carname">Year : 2021 </p>
+                <p class="carname">Fuel type : Diesel</p>
+                <p class="carname">Location : Bangalore <button>view</button></p>
+               
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="innercard1">
+                <img src="car2.jpg" alt="">
+            </div>
+           
+            <div class="innercard2">
+                <p class="carname"><span class="start">BWM</span><span class="end">$1000/day</span></p>
+                <p class="carname">Year : 2021 </p>
+                <p class="carname">Fuel type : Diesel</p>
+                <p class="carname">Location : Bangalore <button>view</button></p>
+                
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="innercard1">
+                <img src="car2.jpg" alt="">
+            </div>
+           
+            <div class="innercard2">
+                <p class="carname"><span class="start">BWM</span><span class="end">$1000/day</span></p>
+                <p class="carname">Year : 2021 </p>
+                <p class="carname">Fuel type : Diesel</p>
+                <p class="carname">Location : Bangalore   <button>view</button></p>
+
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="innercard1">
+                <img src="car2.jpg" alt="">
+            </div>
+           
+            <div class="innercard2">
+                <p class="carname"><span class="start">BWM</span><span class="end">$1000/day</span></p>
+                <p class="carname">Year : 2021 </p>
+                <p class="carname">Fuel type : Diesel</p>
+                <p class="carname">Location : Bangalore  <button>view</button></p>
+
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="innercard1">
+                <img src="car2.jpg" alt="">
+            </div>
+           
+            <div class="innercard2">
+                <p class="carname"><span class="start">BWM</span><span class="end">$1000/day</span></p>
+                <p class="carname">Year : 2021 </p>
+                <p class="carname">Fuel type : Diesel</p>
+                <p class="carname">Location : Bangalore  <button>view</button></p>
+
+            </div>
+        </div>
+
+
+        <div class="card">
+            <div class="innercard1">
+                <img src="car2.jpg" alt="">
+            </div>
+           
+            <div class="innercard2">
+                <p class="carname"><span class="start">BWM</span><span class="end">$1000/day</span></p>
+                <p class="carname">Year : 2021 </p>
+                <p class="carname">Fuel type : Diesel</p>
+                <p class="carname">Location : Bangalore  <button>view</button></p>
+
+            </div>
+        </div>
+
+
+        <div class="card">
+            <div class="innercard1">
+                <img src="car2.jpg" alt="">
+            </div>
+           
+            <div class="innercard2">
+                <p class="carname"><span class="start">BWM</span><span class="end">$1000/day</span></p>
+                <p class="carname">Year : 2021 </p>
+                <p class="carname">Fuel type : Diesel</p>
+                <p class="carname">Location : Bangalore  <button>view</button></p>
+
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="innercard1">
+                <img src="car2.jpg" alt="">
+            </div>
+           
+            <div class="innercard2">
+                <p class="carname"><span class="start">BWM</span><span class="end">$1000/day</span></p>
+                <p class="carname">Year : 2021 </p>
+                <p class="carname">Fuel type : Diesel</p>
+                <p class="carname">Location : Bangalore  <button>view</button></p>
+
+            </div>
+        </div>
+
+    </div>
+
+    <div class="abt">
+        <section id="about">
+        <p id="setpar">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi iure accusamus error sequi beatae ad nostrum voluptates dolorem nam in! Fugit consequuntur, a velit eum sed voluptatum nobis neque obcaecati!
+        Asperiores explicabo, inventore architecto possimus dolorum impedit libero dicta tenetur harum aliquam, minima repellendus alias ut dolore quia commodi consequatur? Dolorum tempore non et fugit deserunt temporibus! Excepturi, animi laborum.
+        Dicta iusto odit enim hic asperiores vel assumenda atque suscipit natus nisi itaque expedita, vitae, nulla blanditiis tenetur magni, eum qui eos illo amet voluptatum voluptate eius ratione voluptatem. Recusandae!
+        Quas doloribus atque architecto. Corrupti cupiditate quisquam facere! Similique odio rerum, delectus beatae iusto alias labore facere ipsum quia est nobis quo amet doloribus. Autem, sint. Ad dolore est suscipit.
+        Vitae facere quas eos at ex modi obcaecati suscipit, totam, harum cumque fuga explicabo culpa consequuntur? Exercitationem, deserunt, ratione, rerum similique incidunt vero recusandae sint in asperiores repellendus vitae aspernatur?</p>
+        </section>
+    </div>
+</body>
+</html>
